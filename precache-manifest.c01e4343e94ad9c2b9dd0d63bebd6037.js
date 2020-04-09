d7a6856d113d38dd1e5d7e64c7b6aedc3607abed
Nuritdinov Iskandar <nuritdinovIskeen@mail.ru>
2020-04-09 13:00:23 +0500

    Updates

---
+++ precache-manifest.c01e4343e94ad9c2b9dd0d63bebd6037.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "224822782e0ef7456748",
+    "url": "/Iskandar-socialMed/static/css/main.69da4695.chunk.css"
+  },
+  {
+    "revision": "224822782e0ef7456748",
+    "url": "/Iskandar-socialMed/static/js/main.22482278.chunk.js"
+  },
+  {
+    "revision": "92188f3648ac52b8ebe6",
+    "url": "/Iskandar-socialMed/static/css/1.30065374.chunk.css"
+  },
+  {
+    "revision": "92188f3648ac52b8ebe6",
+    "url": "/Iskandar-socialMed/static/js/1.92188f36.chunk.js"
+  },
+  {
+    "revision": "49e5f7a41f7de6f274a8",
+    "url": "/Iskandar-socialMed/static/css/2.c9baaacc.chunk.css"
+  },
+  {
+    "revision": "49e5f7a41f7de6f274a8",
+    "url": "/Iskandar-socialMed/static/js/2.49e5f7a4.chunk.js"
+  },
+  {
+    "revision": "c6071d90891d7d8e7e17",
+    "url": "/Iskandar-socialMed/static/js/3.c6071d90.chunk.js"
+  },
+  {
+    "revision": "b5f75394ac6c373522af",
+    "url": "/Iskandar-socialMed/static/js/runtime~main.b5f75394.js"
+  },
+  {
+    "revision": "a3955c529f056cf65e081eac9361f4a4",
+    "url": "/Iskandar-socialMed/static/media/preloader.a3955c52.svg"
+  },
+  {
+    "revision": "86be3d4d46f853110b7d2ac75857acf0",
+    "url": "/Iskandar-socialMed/index.html"
+  }
+];
